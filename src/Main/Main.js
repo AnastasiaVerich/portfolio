ebc594c875d75f7aafa0aadc61b2e30e97ecc1dc
AnastasiaVerich <a.verich.m@gmail.com>
2021-07-15 00:27:27 +0300

    t1

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,34 +5,36 @@ import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-tilt'
 
 
-
- const  particlesOpt ={
-     "particles":{
-         "number":{
-             "value":150,
-             "density": {
-                 "enable": true,
-                 "value_area": 800
-             }
-         }
-     }
- }
+const particlesOpt = {
+    "particles": {
+        "number": {
+            "value": 150,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        links: {
+            color: "rgba(189,109,179,0.58)",
+        },
+    }
+}
 
 function Main() {
     return (
         <div id={"main"} className={style.block}>
-            <Particles className={style.particles} params={particlesOpt} />
+            <Particles className={style.particles} params={particlesOpt}/>
             <div className={style.container}>
                 <div className={style.textContainer}>
                     <span>Привет здесь</span>
-                    <span>Я Анастасия <span >Верич</span></span>
-                    <h1><ReactTypingEffect  text={["Я front-end разработчик"]}/></h1>
+                    <span>Я Анастасия <span>Верич</span></span>
+                    <h1><ReactTypingEffect text={["Я front-end разработчик"]}/></h1>
                 </div>
                 <div className={style.omg}>
-                    <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+                    <Tilt className="Tilt" options={{max: 25}}>
                         <div className="Tilt-inner">
-                    <div className={style.photo}></div>
-                    <span className={style.photoSpan}></span>
+                            <div className={style.photo}></div>
+                            <span className={style.photoSpan}></span>
                         </div>
                     </Tilt>
 
@@ -41,4 +43,5 @@ function Main() {
         </div>
     );
 }
+
 export default Main;
